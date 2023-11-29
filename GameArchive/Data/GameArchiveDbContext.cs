c2be74899464ecb01ee7af90376ab76ac977459f
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2023-11-29 20:41:37 +0100

    Db context created

---
+++ GameArchive/Data/GameArchiveDbContext.cs
@@ -0,0 +1,16 @@
+﻿namespace GameArchive.Data;
+
+using GameArchive.Entities;
+using Microsoft.EntityFrameworkCore;
+
+public class GameArchiveDbContext : DbContext
+{
+    public DbSet<VideoGame> VideoGames => Set<VideoGame>();
+    public DbSet<BoardGame> BoardGames => Set<BoardGame>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseInMemoryDatabase("GameArchiveDb");
+    }
+}
