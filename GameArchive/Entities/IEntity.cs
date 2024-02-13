fa5d1059cc5ae389bf699023f510eda848903488
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2024-02-13 19:16:32 +0100

    class modified, variables moved to child class

--- GameArchive/Entities/IEntity.cs
+++ GameArchive/Entities/IEntity.cs
@@ -3,8 +3,5 @@
 public interface IEntity
 {
     public int Id { get; set; }
-    public string? Name { get; set; }
-    public string? Category { get; set; }
-    public int PublicationYear { get; set; }
-    public string? Producer { get; set; }
+
 }
