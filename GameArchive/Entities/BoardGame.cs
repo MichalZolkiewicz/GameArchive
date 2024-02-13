bc76bde10145fb24304f398a76e5cebebc1034b0
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2024-02-13 19:16:54 +0100

    variables added

--- GameArchive/Entities/BoardGame.cs
+++ GameArchive/Entities/BoardGame.cs
@@ -1,8 +1,12 @@
 ﻿namespace GameArchive.Entities;
 public class BoardGame : EntityBase
 {
+    public string? Name { get; set; }
+    public string? Category { get; set; }
+    public int PublicationYear { get; set; }
+    public string? Producer { get; set; }
     public int MaxPlayers { get; set; }
 
-    public override string ToString() => $"Id: {Id}\nName: {Name}\nCategory: {Category}\nPublication year: {PublicationYear}\nProducer: {Producer}\n Multiplayer option: {MaxPlayers}";
+    public override string ToString() => $"Id: {Id}\nName: {Name}\nCategory: {Category}\nPublication year: {PublicationYear}\nProducer: {Producer}\nMultiplayer option: {MaxPlayers}";
 
 }
--- GameArchive/Entities/VideoGame.cs
+++ GameArchive/Entities/VideoGame.cs
@@ -1,7 +1,11 @@
 ﻿namespace GameArchive.Entities;
 public class VideoGame : EntityBase
 {
-    public string? OnlineOption { get; set; }
+    public string? Name { get; set; }
+    public string? Category { get; set; }
+    public int PublicationYear { get; set; }
+    public string? Producer { get; set; }
+    public Boolean? OnlineOption { get; set; }
 
-    public override string ToString() => $"Id: {Id}\nName: {Name}\nCategory: {Category}\nPublication year: {PublicationYear}\nProducer: {Producer}\n Multiplayer option: {OnlineOption}";
+    public override string ToString() => $"Id: {Id}\nName: {Name}\nCategory: {Category}\nPublication year: {PublicationYear}\nProducer: {Producer}\nMultiplayer option: {OnlineOption}";
 }
