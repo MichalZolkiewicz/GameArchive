f8cb42612adf4d43037f2b3254f6ef8b0d88a923
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2024-02-14 21:03:58 +0100

    clean up

--- GameArchive/Data/GameArchiveDbContext.cs
+++ GameArchive/Data/GameArchiveDbContext.cs
@@ -8,8 +8,8 @@ public class GameArchiveDbContext : DbContext
 {
     public DbSet<VideoGame> VideoGames => Set<VideoGame>();
     public DbSet<BoardGame> BoardGames => Set<BoardGame>();
-
     public string conString = "Data Source=LAPTOP-AJM90S2R;Initial Catalog=GameArchiveDb;Integrated Security=True;Encrypt=False";
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         optionsBuilder.UseSqlServer(conString);
