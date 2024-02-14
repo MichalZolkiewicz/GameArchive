81d1a621a40cbdbb733a37432ab7263a7f3fbc8a
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2024-02-14 13:32:07 +0100

    clean up

--- GameArchive/Program.cs
+++ GameArchive/Program.cs
@@ -2,55 +2,20 @@
 using GameArchive.Entities;
 using GameArchive.Logic;
 using GameArchive.Repositories;
-using GameArchive.Repositories.Extensions;
-
-//var videoGameRepository = new SqlRepository<VideoGame>(new GameArchiveDbContext(), GameAdded);
-//videoGameRepository.ItemAdded += VideoGameRepositoryOnItemAdded;
 
 var boardGameRepository = new SqlRepository<BoardGame>(new GameArchiveDbContext());
-
 var videoGameRepository2 = new SqlRepository<VideoGame>(new GameArchiveDbContext(), GameAdded);
 
-var archiveLogic = new GameArchiveLogic();
+
 
 void VideoGameRepositoryOnItemAdded(object? sender, VideoGame e)
 {
     Console.WriteLine($"Video Game added => {e.Name} from {sender?.GetType().Name}");
 }
 
-//AddVideoGame(videoGameRepository2);
-//WriteAllToConsole(videoGameRepository2);
-
-//SqlRepository<T> chooseRepo<T>(string input)
-//    where T : class, IEntity, new()
-//{
-//    switch(input)
-//    {
-//        case "A":
-//        case "a":
-//            return videoGameRepository;
-//            break;
-//        case "B":
-//        case "b":
-//            return boardGameRepository;
-//        default:
-//            throw new Exception("Wrong letter!");
-//    }
-//}
-
 static void GameAdded(VideoGame item)
 {
     Console.WriteLine($"\n{item.Name} added");
-}
-
-static void WriteAllToConsole(IReadRepository<IEntity> repository)
-{
-    var items = repository.GetAll();
-    foreach (var item in items)
-    {
-        Console.WriteLine();
-        Console.WriteLine(item);
-    }
 }
 
 while (true)
