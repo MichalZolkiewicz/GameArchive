6606c64f1b1d18068c96dcd91619e0829d8944e1
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2024-02-15 20:49:59 +0100

    event logger logic created with two methods

---
+++ GameArchive/Logic/EventLoggerLogic.cs
@@ -0,0 +1,27 @@
+﻿namespace GameArchive.Logic;
+
+public class EventLoggerLogic
+{
+    private const string eventLoggerFile = "eventLogger.txt";
+
+    public static void GameRepositoryOnItemAdded<T>(object? sender, T item)
+    {
+        DateTime localDate = DateTime.Now;
+        string eventLine = localDate.ToString() + "-" + item.GetType().Name + " added";
+        using (var writer = File.AppendText(eventLoggerFile))
+        {
+            writer.WriteLine(eventLine);
+        }
+    }
+
+    public static void GameRepositoryOnItemRemoved<T>(object? sender, T item)
+    {
+        DateTime localDate = DateTime.Now;
+        string eventLine = localDate.ToString() + "-" + item.GetType().Name + " removed";
+        using (var writer = File.AppendText(eventLoggerFile))
+        {
+            writer.WriteLine(eventLine);
+        }
+    }
+
+}
