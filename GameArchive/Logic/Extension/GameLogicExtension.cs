5f8c98b62947fee20225b557e2dde44b0538bdb1
Michał Żółkiewocz <zolkiewicz.michal@gmail.com>
2024-02-27 00:17:15 +0100

    GameLogicExtension class added

---
+++ GameArchive/Logic/Extension/GameLogicExtension.cs
@@ -0,0 +1,32 @@
+﻿namespace GameArchive.Logic.Extension;
+
+public static class GameLogicExtension
+{
+    public static Boolean ConvertStringToBoolean(string input)
+    {
+        if (input == "yes")
+        {
+            return true;
+        }
+        else if (input == "no")
+        {
+            return false;
+        }
+        else
+        {
+            throw new Exception("Incorrect wording!");
+        }
+    }
+
+    public static int ConvertStringToInteger(string input)
+    {
+        if (int.TryParse(input, out int result))
+        {
+            return result;
+        }
+        else
+        {
+            throw new Exception("String is not a integer!");
+        }
+    }
+}
